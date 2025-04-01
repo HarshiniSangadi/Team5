6de769cfe44412053d56e98f0720ee458da5a52c
HarshiniSangadi <152834532+HarshiniSangadi@users.noreply.github.com>
2025-04-01 14:49:32 +0300

    Update forms.py

--- code/health/forms.py
+++ code/health/forms.py
@@ -7,3 +7,11 @@ class DoctorForm(forms.ModelForm):
     class Meta:
         model = Doctor
         exclude = ('status', 'user', 'dob', 'doj')
+
+class AppointmentForm(forms.ModelForm):
+    class Meta:
+        model = Appointment
+        fields = ['doctor', 'scheduled_time']
+        widgets = {
+            'scheduled_time': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
+        }
