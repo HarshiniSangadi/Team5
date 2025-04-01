de40a8755496ec481bd633e96080d9dbd1010557
HarshiniSangadi <152834532+HarshiniSangadi@users.noreply.github.com>
2025-04-01 14:55:01 +0300

    Update urls1.py

--- code/health_desease/urls1.py
+++ code/health_desease/urls1.py
@@ -1,55 +1,70 @@
+"""health_desease URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
 from django.contrib import admin
 from django.urls import path, include
-from accounts.views import user_login, edit
-from django.contrib.auth import views as auth_views
-from devicetoserver import urls
-import devicetoserver, accounts, modelselection
-from serverdata import settings
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
 from django.conf.urls.static import static
-from .api import router
+from health.views import *
+# from .api import router
 from .apirep import routerep
-from allauth.account.views import confirm_email
-from django.views.generic import TemplateView 
-from django.conf.urls import url
 
 urlpatterns = [
-    path('admin/filebrowser/', include('filebrowser.urls')),
-    path('admin/log_viewer/', include('log_viewer.urls')),
-    path('admin/', admin.site.urls), 
-    path('', include('devicetoserver.urls')),
-    path('accounts/', include('accounts.urls')),
-    path('modelselection/', include('modelselection.urls')),    
-    path('computervision/', include('computervision.urls')),
-    path('api/sensoviz/', include(router.urls)),
+    # path('api/sensoviz/', include(router.urls)),
     path('api/v1/', include(routerep.urls)),
-    path('report/', include('fesibility.urls')),
-    path('supportsystem/', include('supportsystem.urls')),
-    path('openvino/', include('openvinofreezed.urls')),
-    path('softwareactivation/', include('softwareactivation.urls')),
-    path('instancesegmentation/', include('instancesegmentation.urls')),
-    path('sensovisionshop/', include('ecommerce_app.urls')),
-    path('paypal/', include('paypal.standard.ipn.urls')),
-    path('inventory/', include('inventory.urls')),
-    path('salerecord/', include('salesapp.urls')),
-
-    url(r'^rest-auth/', include('rest_auth.urls')),
-    url(r'^rest-auth/registration/', include('rest_auth.registration.urls')),
-
-    url(r'^accounts-rest/registration/account-confirm-email/(?P<key>.+)/$', confirm_email, name='account_confirm_email'),
-    path('allaccounts/', include('allauth.urls')),
-    path('hr/', include('hiring.urls')),
-
-    path('account/', include('accountant.urls', namespace='account')),
-#    path('chat/', include('core.urls')),
-    path('analytics/', include('analytics.urls')),
-    url(r'^froala_editor/', include('froala_editor.urls')), 
-    path('ckeditor/', include('ckeditor_uploader.urls')),
-    path('orders/', include('orders.urls')),
+    path('admin/', admin.site.urls),
+    path('', Home, name="home"),
+    # path('patient_home', User_Home,name="patient_home"),
+    path('doctor_home', Doctor_Home,name="doctor_home"),
+    path('admin_home', Admin_Home,name="admin_home"),
+    path('about', About,name="about"),
+    path('contact', Contact,name="contact"),
+    path('gallery', Gallery,name="gallery"),
+    # path('login', Login_User,name="login"),
+    path('login_admin', Login_admin,name="login_admin"),
+    path('signup', Signup_User,name="signup"),
+    path('logout', Logout,name="logout"),
+    path('change_password', Change_Password,name="change_password"),
+    # path('prdict_heart_disease', prdict_heart_disease,name="prdict_heart_disease"),
+    path('add_heartdetail', add_heartdetail,name="add_heartdetail"),
+    path('view_search_pat', view_search_pat,name="view_search_pat"),
     
-]
-urlpatterns += staticfiles_urlpatterns()
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('login/', Login_User, name="login"),
+path('view_patient/', View_Patient, name="view_patient"),
+path('patient_home/', User_Home, name="patient_home"),
+
+    path('view_doctor', View_Doctor,name="view_doctor"),
+    path('add_doctor', add_doctor,name="add_doctor"),
+    path('change_doctor/<int:pid>/', add_doctor,name="change_doctor"),
+    # path('view_patient', View_Patient,name="view_patient"),
+    path('view_feedback', View_Feedback,name="view_feedback"),
+    path('edit_profile', Edit_My_deatail,name="edit_profile"),
+    path('profile_doctor', View_My_Detail,name="profile_doctor"),
+    path('sent_feedback', sent_feedback,name="sent_feedback"),
+
+    path('delete_searched/<int:pid>', delete_searched, name="delete_searched"),
+    path('delete_doctor<int:pid>', delete_doctor, name="delete_doctor"),
+    path('assign_status<int:pid>', assign_status, name="assign_status"),
+    path('delete_patient<int:pid>', delete_patient, name="delete_patient"),
+    path('delete_feedback<int:pid>', delete_feedback, name="delete_feedback"),
+    path('predict_desease/<str:pred>/<str:accuracy>/', predict_desease, name="predict_desease"),
+    
+     path('appointments/', include('health.urls', namespace='appointments')),
+
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 
 
 admin.site.site_header = "Sensovision"
